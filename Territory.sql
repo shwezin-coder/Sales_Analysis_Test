7b8dff37dd0575559df7854bb536222e47b6d399
Daniel Hardin <70483157+shwezin-coder@users.noreply.github.com>
2023-12-19 16:15:40 +0630

    Add files via upload

---
+++ Territory.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [AdventureWorks_Territory] 
+(
+    [SalesTerritoryKey]	INT,
+    [Region]	VARCHAR(512),
+    [Country]	VARCHAR(512),
+    [Continent]	VARCHAR(512)
+);
+
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('1', 'Northwest', 'United States', 'North America');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('2', 'Northeast', 'United States', 'North America');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('3', 'Central', 'United States', 'North America');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('4', 'Southwest', 'United States', 'North America');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('5', 'Southeast', 'United States', 'North America');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('6', 'Canada', 'Canada', 'North America');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('7', 'France', 'France', 'Europe');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('8', 'Germany', 'Germany', 'Europe');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('9', 'Australia', 'Australia', 'Pacific');
+INSERT INTO [AdventureWorks_Territory] ([SalesTerritoryKey], [Region], [Country], [Continent]) VALUES ('10', 'United Kingdom', 'United Kingdom', 'Europe');
