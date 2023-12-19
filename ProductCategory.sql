475f5091c0c026beaed4340446e5f58aaa488f79
Daniel Hardin <70483157+shwezin-coder@users.noreply.github.com>
2023-12-19 16:15:11 +0630

    Add files via upload

---
+++ ProductCategory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [AdventureWorks_ProductCategory] 
+(
+    [ProductCategoryKey]	INT,
+    [CategoryName]	VARCHAR(512)
+);
+
+INSERT INTO [AdventureWorks_ProductCategory] ([ProductCategoryKey], [CategoryName]) VALUES ('1', 'Bikes');
+INSERT INTO [AdventureWorks_ProductCategory] ([ProductCategoryKey], [CategoryName]) VALUES ('2', 'Components');
+INSERT INTO [AdventureWorks_ProductCategory] ([ProductCategoryKey], [CategoryName]) VALUES ('3', 'Clothing');
+INSERT INTO [AdventureWorks_ProductCategory] ([ProductCategoryKey], [CategoryName]) VALUES ('4', 'Accessories');
---
+++ ProductSubCategory.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE [AdventureWorks_ProductSubCategory] 
+(
+    [ProductSubcategoryKey]	INT,
+    [SubcategoryName]	VARCHAR(512),
+    [ProductCategoryKey]	INT
+);
+
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('1', 'Mountain Bikes', '1');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('2', 'Road Bikes', '1');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('3', 'Touring Bikes', '1');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('4', 'Handlebars', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('5', 'Bottom Brackets', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('6', 'Brakes', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('7', 'Chains', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('8', 'Cranksets', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('9', 'Derailleurs', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('10', 'Forks', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('11', 'Headsets', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('12', 'Mountain Frames', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('13', 'Pedals', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('14', 'Road Frames', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('15', 'Saddles', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('16', 'Touring Frames', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('17', 'Wheels', '2');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('18', 'Bib-Shorts', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('19', 'Caps', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('20', 'Gloves', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('21', 'Jerseys', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('22', 'Shorts', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('23', 'Socks', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('24', 'Tights', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('25', 'Vests', '3');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('26', 'Bike Racks', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('27', 'Bike Stands', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('28', 'Bottles and Cages', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('29', 'Cleaners', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('30', 'Fenders', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('31', 'Helmets', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('32', 'Hydration Packs', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('33', 'Lights', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('34', 'Locks', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('35', 'Panniers', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('36', 'Pumps', '4');
+INSERT INTO [AdventureWorks_ProductSubCategory] ([ProductSubcategoryKey], [SubcategoryName], [ProductCategoryKey]) VALUES ('37', 'Tires and Tubes', '4');
